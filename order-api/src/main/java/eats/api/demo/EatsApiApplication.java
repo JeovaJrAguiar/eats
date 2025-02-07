5869ff0a9d25ce42a48ff14bcd9fad1407637d91
Lucas <soares0lucas8@gmail.com>
2025-02-07 19:21:03 -0300

    feat: estrutura inicial da api

---
+++ order-api/src/main/java/eats/api/demo/EatsApiApplication.java
@@ -0,0 +1,13 @@
+package eats.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatsApiApplication.class, args);
+	}
+
+}
