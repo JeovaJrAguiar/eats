e703ebd6f86756050d0d4e93515a9eaef0654942
Lucas <soares0lucas8@gmail.com>
2025-02-08 22:10:51 -0300

    feat: estrutura inicial do modulo de notificações

---
+++ Notification/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+int porta = 5000;
+TcpListener server = new(IPAddress.Any, porta);
+server.Start();
+
+while (true)
+{
+	TcpClient cliente = server.AcceptTcpClient();
+	NetworkStream stream = cliente.GetStream();
+	byte[] buffer = new byte[1024];
+
+	int bytesRead = stream.Read(buffer, 0, buffer.Length);
+	string messageReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+	Console.WriteLine($"Notificação recebida: {messageReceived}");
+
+	string response = "Notificação recebida com sucesso!";
+	byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+	stream.Write(responseBytes, 0, responseBytes.Length);
+
+	cliente.Close();
+}
