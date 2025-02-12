f707c81304c6197c0908943f73f851ec30c50cd4
Vicente Neto <31258486+VicenteNeto21@users.noreply.github.com>
2025-02-12 02:16:37 -0300

    Criação de uma homepage/inicio temporario

--- eats-front/src/app/app.routes.ts
+++ eats-front/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {loginGuard} from './pages/login/login.guard';
 import {LoginComponent} from './pages/login/login.component';
 import {authGuard} from './core/guards/auth.guard';
 import {DemandComponent} from './pages/demand/demand.component';
-import { QuiosqueComponent } from './pages/quiosque/quiosque.component';
+import {QuiosqueComponent} from './pages/quiosque/quiosque.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomepageComponent},
   { path: 'login', component: LoginComponent },
   { path: 'pedido', component: DemandComponent },
   { path: 'quiosque', component: QuiosqueComponent},
---
+++ eats-front/src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [CommonModule],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+  userName: string = 'Vicente Neto'; // Substitua com o nome real do usuário logado
+
+  categories = [
+    { name: 'Comida', image: 'https://travel-guide.daytours4u.com/pt/wp-content/uploads/sites/4/2019/11/comidaperuana1.jpg' },
+    { name: 'Supermercados', image: 'https://www.sde.ce.gov.br/wp-content/uploads/sites/15/2022/06/240502680_4608349989189306_5484089748816698579_n.jpg' },
+    { name: 'Farmácias', image: 'https://revistadafarmacia.com.br/wp-content/uploads/2021/06/Farmacia-4-1-1024x544.jpg' },
+    { name: 'Bebidas', image: 'https://blog.webbar.com.br/wp-content/uploads/2023/07/159304.png' }
+  ];
+
+  specificCategories = [
+    { name: 'Lanches', image: 'https://via.placeholder.com/150' },
+    { name: 'Promoções', image: 'https://via.placeholder.com/150' },
+    { name: 'Pizza', image: 'https://via.placeholder.com/150' },
+    { name: 'Açaí', image: 'https://via.placeholder.com/150' },
+    { name: 'Bebidas', image: 'https://via.placeholder.com/150' },
+    { name: 'Brasileira', image: 'https://via.placeholder.com/150' },
+    { name: 'Japonesa', image: 'https://via.placeholder.com/150' },
+    { name: 'Pastel', image: 'https://via.placeholder.com/150' }
+  ];
+}
