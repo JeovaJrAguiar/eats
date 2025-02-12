d1145389dabe2eb4893a61fc4c53c34f2cc98bc5
JeovaJrAguiar <aguiarjr897@gmail.com>
2025-02-11 22:08:39 -0300

    fix: Corrige gerencialmento de rotas

---
+++ eats-front/src/app/core/services/index.ts
@@ -0,0 +1,5 @@
+export { AuthService } from  './auth.service'
+export { CookieService } from  './cookie.service'
+export { SessionStorageService } from  './session-storage.service'
+export { StorageService } from  './storage.service'
+
--- eats-front/src/app/core/tokens/storage.token.ts
+++ eats-front/src/app/core/tokens/storage.token.ts
@@ -1,6 +1,7 @@
-import {InjectionToken} from '@angular/core';
+import {inject, InjectionToken, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
 export const SESSION_STORAGE = new InjectionToken<Storage>('Session Storage', {
-  providedIn: "root",
+  providedIn: 'root',
   factory: () => sessionStorage,
 });
