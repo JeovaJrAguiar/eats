f5e8489c562bed2913b66095d18c84163232bdfb
JeovaJrAguiar <aguiarjr897@gmail.com>
2025-02-11 22:09:28 -0300

    feat: Adiciona novas paginas

---
+++ eats-front/src/app/pages/demand/demand.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demand',
+  imports: [],
+  templateUrl: './demand.component.html',
+  styleUrl: './demand.component.css'
+})
+export class DemandComponent {
+
+}
