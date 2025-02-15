d0b2dbb1ac03cb0a7f863475437c3cdbf043992e
Lucas <soares0lucas8@gmail.com>
2025-02-14 22:52:14 -0300

    chore: configurando cors

--- order-api/src/main/java/eats/api/demo/controllers/OrderController.java
+++ order-api/src/main/java/eats/api/demo/controllers/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@CrossOrigin(origins = "http://localhost:4200")
 public class OrderController {
     @Autowired
     private OrderService orderService;
--- order-api/src/main/java/eats/api/demo/security/SecurityConfig.java
+++ order-api/src/main/java/eats/api/demo/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
                         .anyRequest().authenticated())
